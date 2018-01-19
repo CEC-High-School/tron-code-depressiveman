d71a666bc0219aee065f9cd8d656b4b6da967798
Dr. J. Moriarty <Jennifer_Moriarty@dpsk12.org>
2018-01-19 07:28:10 -0700

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,1 +1,10 @@
-//write a program that prints a corny joke or pun to the console window :D
+
+
+//instructions for 1-19
+//
+//intro: Write a program that asks a user for how old they are, and then repeats that number back to them in an insult.
+//
+//intermediate: Write a program that prints the numbers 256 down to 32, counting by fours
+//
+//advanced: make an account at HackerRank.com
+//do this problem: https://www.hackerrank.com/challenges/c-tutorial-struct/problem
